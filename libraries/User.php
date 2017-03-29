cea1f88c49a1a91f68fa9f7147007d99a199d7ae
georgesshilisia <georgesshilisia@gmail.com>
2017-03-29 16:34:53 +0300

    Third Commit

--- libraries/User.php
+++ libraries/User.php
@@ -2,8 +2,38 @@
 class User
 {
     /*
+     * Init DB Variable
+     */
+    private $db;
+    /*
+     * Constructor
+     */
+    public function __construct(){
+        $this->db = new Database();
+}
+    /*
      * Register User
      */
+    function register($data){
+        $this->db->query('INSERT INTO users (name, email, avatar, username, password, about,last_activity)
+                                      VALUES (:name, :email, :avatar, :username, :password, :about, :last_activity))');
+        //Bind Values
+        $this->db->bind(':name', $data['name']);
+        $this->db->bind(':email', $data['email']);
+        $this->db->bind(':avatar', $data['avatar']);
+        $this->db->bind(':username', $data['username']);
+        $this->db->bind(':password', $data['password']);
+        $this->db->bind(':about', $data['about']);
+        $this->db->bind(':last_activity', $data['last_activity']);
+
+        //Execute
+        if($this->db->execute()){
+            return true;
+        } else{
+            return false;
+        }
+        //echo $this->db->lastInsertId();
+    }
 
      /*
      * Upload User Avatar
